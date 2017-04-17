8ddb8ba27636a029b083726554bf446243614ad6
Joshua Hawxwell <m@hawx.me>
2017-04-17 15:49:57 +0100

    Remove edit/delete from list view

--- web/views/list.go
+++ web/views/list.go
@@ -14,13 +14,6 @@ const list = `<!DOCTYPE html>
         <li class="repo {{if .IsPrivate}}private{{end}}">
           <div class="repo-header">
             <h1><a href="/{{.Name}}">{{.Name}}</a></h1>
-
-            {{ if $.LoggedIn }}
-            <div class="buttons">
-              <a href="/{{.Name}}/edit">edit</a>
-              <a href="/{{.Name}}/delete">delete</a>
-            </div>
-            {{ end }}
           </div>
 
           <p>{{.Description}}</p>
