a3319877e758470ad837b2e15f4e5870f1d93047
Joshua Hawxwell <m@hawx.me>
2022-08-21 18:25:39 +0100

    Don't log every page view

--- ggg.go
+++ ggg.go
@@ -109,12 +109,5 @@ func main() {
 
 	route.Handle("/public/*path", http.StripPrefix("/public", http.FileServer(http.Dir(*webPath+"/static"))))
 
-	serve.Serve(*port, *socket, Log(route.Default))
-}
-
-func Log(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s", r.Method, r.URL)
-		handler.ServeHTTP(w, r)
-	})
+	serve.Serve(*port, *socket, route.Default)
 }
